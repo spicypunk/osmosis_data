7a4d86535a5d02c5d44219549d818f1d861b445a
spicypunk <bethgenewang@gmail.com>
2023-03-22 16:59:23 -0400

    updated multiprocessing

--- binance_order_book.py
+++ binance_order_book.py
@@ -7,11 +7,18 @@ import numpy as np
 import datetime
 import time
 import pytz
+import multiprocessing
 from multiprocessing import Process, Queue, Value, Array, Pool
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth', 30)
+
 
 ### Rationale: Python
 # keep getting orderbook from binance,
-def get_binance_book():
+def get_binance_book(queue):
     rest_url = f"https://data.binance.com/api/v3/depth"
 
     params = {
@@ -23,32 +30,27 @@ def get_binance_book():
         snapshot = requests.get(rest_url, params=params).json()
     except requests.exceptions.JSONDecodeError as e:
         snapshot = None
-        return snapshot
     # a dictionary of side: price, quantity orderbook
     snapshot["time"] = get_gmt_time()
-    return snapshot
+    queue.put(snapshot)
 
-def get_block_time():
+def get_block_time(queue):
     try:
         block_request = requests.get(
             'https://osmosis-lcd.quickapi.com/cosmos/base/tendermint/v1beta1/blocks/latest').json()
+        time.sleep(2)
     except requests.exceptions.JSONDecodeError as e:
+        # print("Error: ", e)
         blockTime = None
         blockNumber = None
-        return blockTime, blockNumber
+    
 
     blockNumber = int(block_request['block']['header']['height'])
     blockTime = block_request['block']['header']['time']
     blockTime = blockTime.replace('Z', '')[0:26]
     blockTime = datetime.datetime.strptime(blockTime, "%Y-%m-%dT%H:%M:%S.%f")
-    # print(type(blockTime))
 
-    # except Exception as e:
-    #     print(f"an error retrieving blocktime has occured: {e}" )
-    #     if blockTime is None:
-    #         raise ValueError('Failed to get block time')
-
-    return blockTime, blockNumber
+    queue.put((blockNumber, blockTime))
 
 
 def get_gmt_time():
@@ -64,38 +66,60 @@ def get_gmt_time():
 #     return datetime1 < datetime2
 
 def main():
+    result_queue = multiprocessing.Queue()
+
+    # create two processes to execute the API queries
+    book_process = multiprocessing.Process(target=get_binance_book, args=(result_queue,))
+    block_process = multiprocessing.Process(target=get_block_time, args=(result_queue,))
+
+        # start the processes
+    book_process.start()
+    block_process.start()
+
+    # wait for the processes to complete
+    book_process.join()
+    block_process.join()
+
+
     sleep_time = 0.5
 
     df_osmosis = pd.DataFrame(columns=['block_time', 'block_height'])
-    block_time, block_height = get_block_time()
-    current_update = {'block_time': block_time, 'block_height': block_height}
-    df_osmosis = df_osmosis.append(current_update, ignore_index=True)
+    
+    if not result_queue.empty():
+        block_info = result_queue.get()
+        print(f"the block_info is {block_info}.")
+        block_time = block_info[0]
+        block_height = block_info[1]
+        current_update = {'block_time': block_time, 'block_height': block_height}
+        df_osmosis = df_osmosis.append(current_update, ignore_index=True)
 
-    df_binance = pd.DataFrame(columns=['bids', 'asks', 'lastUpdateId', 'time', 'flag'])
-    binance_snapshot = get_binance_book()
-    df_binance = df_binance.append(binance_snapshot, ignore_index=True)
+        df_binance = pd.DataFrame(columns=['bids', 'asks', 'lastUpdateId', 'time', 'flag'])
+        binance_snapshot = result_queue.get()
+        df_binance = df_binance.append(binance_snapshot, ignore_index=True)
 
     while True:
-        block_time, block_height = get_block_time()
-        # if block_time is the same as last time (during sleep_time, no block is formed) or if block_time returns None due to Server issue
-        if block_time == df_osmosis.iloc[-1]['block_time'] or block_time == None:
-            time.sleep(sleep_time)
-            continue
-        # if block_time==df['block_time'].iloc[-1] and block_height==df['block_height'].iloc[-1]:
-        current_update = {'block_time': block_time, 'block_height': block_height} # type = dict
-        df_osmosis = df_osmosis.append(current_update, ignore_index=True)
-        # df_osmosis.drop_duplicates(subset=None,keep='last',inplace=True)
-        print(df_osmosis)
-
-        binance_snapshot = get_binance_book()
+        binance_snapshot = result_queue.get()
+        print(binance_snapshot)
         df_binance = df_binance.append(binance_snapshot, ignore_index=True)
+        # print(df_binance)
         # df_binance.loc[-1].at['flag'] = False
+        if not result_queue.empty():
+            block_info = result_queue.get()
+            block_time = block_info[0]
+            block_height = block_info[1]
+            # if block_time is the same as last time (during sleep_time, no block is formed) or if block_time returns None due to Server issue
+            if block_time == df_osmosis.iloc[-1]['block_time'] or block_time == None:
+                continue
+            # if block_time==df['block_time'].iloc[-1] and block_height==df['block_height'].iloc[-1]:
+            current_update = {'block_time': block_time, 'block_height': block_height} # type = dict
+            df_osmosis = df_osmosis.append(current_update, ignore_index=True)
+        # df_osmosis.drop_duplicates(subset=None,keep='last',inplace=True)
+        # print(df_osmosis)
 
-        print(df_binance)
         # get the subset of df_binance where time < block_time
         subset = df_binance.loc[df_binance['time'] < block_time]
         try:
-            max_time_idx = subset['time'].idxmax()
+            max_time_idx = subset['time'].idxmax() 
             df_binance.at[max_time_idx, 'flag'] = True
             print(max_time_idx)
         except:
@@ -104,9 +128,4 @@ def main():
         time.sleep(sleep_time)
 
 if __name__ == "__main__":
-    # with Pool(processes=2) as pool:
-    #     pool.map(main, range(10))
-    #     q = Queue()
-    p = Process(target=main, args=())
-    p.start()
-    p.join()
+    main()
